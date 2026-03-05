86588a20bd17c50671c9a7603a38427bc0000895
nadznyu <nadiaalya0107@gmail.com>
2026-03-05 18:44:18 +0700

    Modifikasi Percobaan 3.4 No. 4 kode program (Input)

--- MataKuliahDemo16.java
+++ MataKuliahDemo16.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 public class MataKuliahDemo16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliah16 [] arrayOfMataKuliah16 = new MataKuliah16[3];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan jumlah Matakuliah : " );
+        dummy = sc.nextLine();
+        int jumlahData = Integer.parseInt(dummy);
+        System.out.println("----------------------------");
 
-            arrayOfMataKuliah16[i] = new MataKuliah16("-", "-", 0, 0);
+        MataKuliah16 [] arrayOfMataKuliah16 = new MataKuliah16[jumlahData];
+
+        for (int i = 0; i < jumlahData; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
             System.out.print("Kode        : ");
             kode = sc.nextLine();
@@ -22,10 +26,10 @@ public class MataKuliahDemo16 {
             jumlahJam = Integer.parseInt(dummy);
             System.out.println("-------------------------------");
 
-            arrayOfMataKuliah16[i].tambahData(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah16[i] = new MataKuliah16(kode, nama, sks, jumlahJam);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahData; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMataKuliah16[i].cetakInfo();
 
